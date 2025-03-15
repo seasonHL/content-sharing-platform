c6c558c87424092eb0556ec6a9bb83cfed98461d
season <3090331318@qq.com>
2025-03-15 14:39:37 +0800

    feat: 图片上传优化

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import * as path from 'path';
 import * as crypto from 'crypto';
 import { MediaService } from 'src/media/media.service';
@@ -8,16 +8,14 @@ import { cos } from 'src/config/cos';
 
 @Controller('upload')
 export class UploadController {
+    bucket = 'season-1313247063';
+    region = 'ap-chengdu';
     constructor(
         private readonly mediaService: MediaService,
     ) { }
-    @Post('image')
-    @UseInterceptors(FileInterceptor('image', {
-        limits: { fileSize: 1024 * 1024 * 5 }, // 限制文件大小为 5MB
-    }))
-    async uploadCos(@UploadedFile() file: Express.Multer.File) {
-        const bucket = 'season-1313247063';
-        const region = 'ap-chengdu';
+    uploadCos = async (file: Express.Multer.File) => {
+        const bucket = this.bucket;
+        const region = this.region;
         const md5Hash = crypto.createHash('md5').update(file.buffer).digest('hex');
         const key = `uploads/${md5Hash}${path.extname(file.originalname)}`;
 
@@ -31,12 +29,27 @@ export class UploadController {
                 if (err) {
                     reject(err);
                 } else {
-                    const url = `https://${bucket}.cos.${region}.myqcloud.com/${key}`;
-                    resolve(successResponse({
-                        url,
-                    }));
+                    resolve(key)
                 }
-            });
-        });
+            })
+        })
     }
+
+    @Post('image')
+    @UseInterceptors(FileInterceptor('image', {
+        limits: { fileSize: 1024 * 1024 * 5 }, // 限制文件大小为 5MB
+    }))
+    async uploadImage(@UploadedFile() file: Express.Multer.File) {
+        const url = await this.uploadCos(file);
+        return successResponse(url);
+    }
+
+
+    @Post('images')
+    @UseInterceptors(FilesInterceptor('images'))
+    async uploadImages(@UploadedFiles() files: Array<Express.Multer.File>) {
+        const res = await Promise.all(files.map((file) => this.uploadCos(file)));
+        return successResponse(res);
+    }
+
 }
